bee2b5480e0ccda683c898d9fff60f68315b85e0
PrincessPi3 <human.bagel@gmail.com>
2025-06-18 07:03:31 -0600

    forgot to disable the stupidntesting shit lmfao

--- bd_crack.sh
+++ bd_crack.sh
@@ -47,4 +47,4 @@ else # if charset is not specified default to digit, upper alpha, lower alpha, a
     charset_formatted="?l?u?d?s"
 fi
 
-echo hashcat -a3 -m100 $1 -1 $charset_formatted "$2?1?1?1?1?1?1?1?1" --increment --increment-min=5
+hashcat -a3 -m100 $1 -1 $charset_formatted "$2?1?1?1?1?1?1?1?1" --increment --increment-min=5
